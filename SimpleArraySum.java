43b53d79d52582deb99da85d7101c7bd9ddb4caa
Jason <jason.moyer91@gmail.com>
2016-03-13 20:51:11 -0400

    Create SimpleArraySum.java

---
+++ SimpleArraySum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class SimpleArraySum {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        final int n = stdin.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += stdin.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
